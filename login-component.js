166121863465b553ebfdc726553548c2f9237f7f
Bonds95 <sergeybondarenko95@yandex.ru>
2023-06-08 18:10:40 +0300

    добавлена форма регистрации

--- login-component.js
+++ login-component.js
@@ -1,41 +1,59 @@
 import { loginApi } from "./api.js";
 
 export function renderLoginComponent({ appEl, fetchTodosAndRender, setToken }) {
-    let appHtml = `<div class="form">
-            <h3 class="form-title">Форма авторизации</h3>
-            <div class="form-row">
-            Логин:
-            <input type="text" id="login-input" class="input" placeholder="Выпить кофе" />
-            <br>
-            Пароль:
-            <input type="password" id="password-input" class="input" placeholder="Выпить кофе" />
-            </div>
-            <br>
-            <button class="button" id="login-button">Войти</button>`
-    appEl.innerHTML = appHtml;
 
-    document.getElementById('login-button').addEventListener('click', () => {
-        const login = document.getElementById('login-input').value
-        const password = document.getElementById('password-input').value
+    let isLoginMode = true
 
-        if (!login) {
-            alert('Введите логин')
-            return
-        }
+    const renderForm = () => {
+        let appHtml = `<div class="form">
+        <h3 class="form-title">Форма ${isLoginMode ? 'входа' : 'регистрации'}</h3>
+        <div class="form-row">
+        ${isLoginMode ? '' : `Имя:
+        <input type="text" id="name-input" class="input" placeholder="Введите имя" />
+        <br>`}
         
-        if (!password) {
-            alert('Введите пароль')
-            return
-        }
-        loginApi({
-            login: login,
-            password: password
-        }).then((user) => {
-            setToken(`Bearer ${user.user.token}`)
-            fetchTodosAndRender()
-        }).catch(error=> {
-            alert(error.message)
-            
+        Логин:
+        <input type="text" id="login-input" class="input" placeholder="Введите логин" />
+        <br>
+        Пароль:
+        <input type="password" id="password-input" class="input" placeholder="Введите пароль" />
+        </div>
+        <br>
+        <button class="button" id="login-button">${isLoginMode ? "Войти" : "Зарегистрироваться"}</button>
+        <br>
+        <br>
+        <button class="button" id="toggle-button">Перейти к ${isLoginMode ? "регистрации" : "входу"}</button>`
+        appEl.innerHTML = appHtml;
+
+        document.getElementById('login-button').addEventListener('click', () => {
+            const login = document.getElementById('login-input').value
+            const password = document.getElementById('password-input').value
+
+            if (!login) {
+                alert('Введите логин')
+                return
+            }
+
+            if (!password) {
+                alert('Введите пароль')
+                return
+            }
+            loginApi({
+                login: login,
+                password: password
+            }).then((user) => {
+                setToken(`Bearer ${user.user.token}`)
+                fetchTodosAndRender()
+            }).catch(error => {
+                alert(error.message)
+
+            })
         })
-    })
+        document.getElementById('toggle-button').addEventListener('click', () => {
+            isLoginMode = !isLoginMode
+            renderForm()
+        })
+    }
+
+renderForm()
 }
